02cbe8829b4626b98a249d240961905f9a2f0215
AdjieAdrianSaputra <adjieadrian12@gmail.com>
2024-06-14 16:58:38 +0700

    menampilkan gambar produk dan stylenya

--- resources/views/barang/daftar_barang.blade.php
+++ resources/views/barang/daftar_barang.blade.php
@@ -1,24 +1,46 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Daftar Barang</title>
 
-@section('content')
+    <style>
+      .card-img-top {
+          width: 100%;
+          height: 200px;
+          object-fit: fill;
+      }
+  </style>
+
+</head>
+<body>
 <div class="container">
-  <a href="{{ route('barang.tambah_barang') }}" class="btn btn-success mt-3">Tambah Daftar Barang</a>
+    <h1>Daftar Barang</h1>
+    <a href="{{ route('barang.tambah_barang') }}" class="btn btn-success mb-3">Tambah Daftar Barang</a>
     <div class="row">
-
-      @foreach($produks as $item)
-      <div class="col-md-3 mt-4">
-        <div class="card border-dark">
-          <img src="{{ asset('uploads/' . $item->gambar) }}" class="card-img-top" alt="...">
-          <div class="card-body">
-            <h5 class="card-title font-weight-bold">{{ $item->nama_barang }}</h5>
-            <label class="card-text harga"><strong>Rp.</strong> {{ number_format($item->harga) }}</label><br>
-            <a href="('edit_barang', $item->id) }}" class="btn btn-success btn-sm btn-block">EDIT</a>
-            <a href="('hapus_barang', $item->id) }}" class="btn btn-danger btn-sm btn-block text-light">HAPUS</a>
-          </div>
-        </div>
-      </div>
-      @endforeach
-
+        @foreach($produks as $produk)
+            <div class="col-md-4">
+                <div class="card mb-4">
+                    <img src="{{ asset('storage/' . $produk->gambar) }}" class="card-img-top" alt="Gambar Barang">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $produk->nama_barang }}</h5>
+                        <p class="card-text">Rp. {{ $produk->harga }}</p>
+                        <a href="{{ route('edit_barang', $produk->id) }}" class="btn btn-primary">Edit</a>
+                        <form action="{{ route('hapus_barang', $produk->id) }}" method="POST" class="d-inline">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger">Hapus</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
     </div>
 </div>
-@endsection
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
